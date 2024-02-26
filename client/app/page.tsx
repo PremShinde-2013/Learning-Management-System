5151c2d57d72d27fc1abb8eee899b9f85fc3212a
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-02-27 00:04:02 +0530

    added sign in form

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,53 +6,70 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import { SparklesPreview } from "./components/sparkles";
+import { Image } from "@nextui-org/react";
+import { Container } from "postcss";
 
 export default function Home() {
 	return (
 		<>
-			<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
-				<div className='inline-block max-w-lg text-center justify-center'>
-					<h1 className={title()}>Make&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-					<br />
-					<h1 className={title()}>
-						websites regardless of your design experience.
-					</h1>
-					<h2 className={subtitle({ class: "mt-4" })}>
-						Beautiful, fast and modern React UI library.
-					</h2>
-				</div>
+			<div
+				className='container mx-auto  
+			flex lg:flex-row flex-col justify-evenly'
+			>
+				<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10 '>
+					<Image
+						isZoomed
+						width={240}
+						height={240}
+						alt='NextUI Fruit Image with Zoom'
+						src='https://images.unsplash.com/photo-1708374580439-553f21a95345?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEzfEpwZzZLaWRsLUhrfHxlbnwwfHx8fHw%3D'
+					/>
+				</section>
+				<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
+					<div className='inline-block max-w-lg text-center justify-center'>
+						<h1 className={title()}>Make&nbsp;</h1>
+						<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+						<br />
+						<h1 className={title()}>
+							websites regardless of your design experience.
+						</h1>
+						<h2 className={subtitle({ class: "mt-4" })}>
+							Beautiful, fast and modern React UI library.
+						</h2>
+					</div>
 
-				<div className='flex gap-3'>
-					<Link
-						isExternal
-						href={siteConfig.links.docs}
-						className={buttonStyles({
-							color: "primary",
-							radius: "full",
-							variant: "shadow",
-						})}
-					>
-						Documentation
-					</Link>
-					<Link
-						isExternal
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
-				</div>
+					<div className='flex gap-3'>
+						<Link
+							isExternal
+							href={siteConfig.links.docs}
+							className={buttonStyles({
+								color: "primary",
+								radius: "full",
+								variant: "shadow",
+							})}
+						>
+							Documentation
+						</Link>
+						<Link
+							isExternal
+							className={buttonStyles({ variant: "bordered", radius: "full" })}
+							href={siteConfig.links.github}
+						>
+							<GithubIcon size={20} />
+							GitHub
+						</Link>
+					</div>
 
-				<div className='mt-8'>
-					<Snippet hideSymbol hideCopyButton variant='flat'>
-						<span>
-							Get started by editing <Code color='primary'>app/page.tsx</Code>
-						</span>
-					</Snippet>
-				</div>
-			</section>
+					<div className='mt-8'>
+						<Snippet hideSymbol hideCopyButton variant='flat'>
+							<span>
+								Get started by editing <Code color='primary'>app/page.tsx</Code>
+							</span>
+						</Snippet>
+					</div>
+				</section>
+			</div>
+
 			<SparklesPreview />
 		</>
 	);
--- client/components/navbar.tsx
+++ client/components/navbar.tsx
@@ -14,6 +14,21 @@ import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
 import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
 import { link as linkStyles } from "@nextui-org/theme";
+import FaceIcon from "@mui/icons-material/Face";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import GoogleIcon from "@mui/icons-material/Google";
+import {
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	useDisclosure,
+	Checkbox,
+} from "@nextui-org/react";
+import { MailIcon } from "./icons";
+import { LockIcon } from "./icons";
 
 import { siteConfig } from "@/config/site";
 import NextLink from "next/link";
@@ -30,9 +45,18 @@ import {
 } from "@/components/icons";
 
 import { Logo } from "@/components/icons";
+import { ModalFooter } from "@nextui-org/react";
+import { useState } from "react";
 
 export const Navbar = () => {
 	const pathname = usePathname();
+
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+	const [visible, setVisible] = useState(false);
+	const [password, setPassword] = useState("");
+	const toggleVisibility = () => {
+		setVisible(!visible);
+	};
 
 	const searchInput = (
 		<Input
@@ -109,12 +133,14 @@ export const Navbar = () => {
 					</Button>
 				</NavbarItem>
 				<NavbarItem className='hidden md:flex'>
-					<Avatar
-						className='hidden lg:flex'
-						isBordered
-						radius='full'
-						src='https://i.pravatar.cc/150?u=a04258114e29026708c'
-					/>
+					<Button onPress={onOpen} color='default' className='bg-transparent'>
+						<Avatar
+							className='hidden lg:flex'
+							isBordered
+							radius='full'
+							src='https://i.pravatar.cc/150?u=a04258114e29026708c'
+						/>{" "}
+					</Button>
 				</NavbarItem>
 			</NavbarContent>
 
@@ -152,6 +178,174 @@ export const Navbar = () => {
 					))}
 				</div>
 			</NavbarMenu>
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='top-center'>
+				<ModalContent>
+					{(onClose) => (
+						<>
+							<ModalHeader className='flex flex-col gap-1'>Log in</ModalHeader>
+							<ModalBody>
+								<Input
+									autoFocus
+									endContent={
+										<FaceIcon className='text-2xl text-default-400 pointer-events-none flex-shrink-0' />
+									}
+									label='Name'
+									placeholder='Enter your name'
+									variant='bordered'
+								/>
+								<Input
+									autoFocus
+									endContent={
+										<MailIcon className='text-2xl text-default-400 pointer-events-none flex-shrink-0' />
+									}
+									label='Email'
+									placeholder='Enter your email'
+									variant='bordered'
+								/>
+								<Input
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									endContent={
+										visible ? (
+											<VisibilityRoundedIcon
+												className='text-2xl text-default-400 pointer-events-none flex-shrink-0'
+												onClick={toggleVisibility}
+											/>
+										) : (
+											<VisibilityOffRoundedIcon
+												className='text-2xl text-default-400 pointer-events-none flex-shrink-0'
+												onClick={toggleVisibility}
+											/>
+										)
+									}
+									label='Password'
+									placeholder='Enter your password'
+									type={visible ? "text" : "password"}
+									variant='bordered'
+								/>
+								<div className='flex py-2 px-1 justify-between'>
+									<Checkbox
+										classNames={{
+											label: "text-small",
+										}}
+										onClick={toggleVisibility}
+									>
+										Show Password
+									</Checkbox>
+									<Link color='primary' href='#' size='sm'>
+										Forgot password?
+									</Link>
+								</div>
+								<Button color='primary' variant='shadow' onPress={onClose}>
+									Sign in
+								</Button>
+								<div className='flex py-2 px-1 justify-center gap-4'>
+									<h1> or join us with</h1>
+								</div>
+
+								<div className='flex py-2 px-1 justify-center gap-4'>
+									<GoogleIcon />
+									<GitHubIcon />
+								</div>
+								<div className='flex py-2 px-1 justify-center gap-4'>
+									<h1>Already have an account?</h1>
+									<Link onClick={onOpen}>Sign in</Link>
+								</div>
+							</ModalBody>
+
+							<ModalFooter>
+								<Button color='danger' variant='flat' onPress={onClose}>
+									Close
+								</Button>
+							</ModalFooter>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='top-center'>
+				<ModalContent>
+					{(onClose) => (
+						<>
+							<ModalHeader className='flex flex-col gap-1'>Sign in</ModalHeader>
+							<ModalBody>
+								<Input
+									autoFocus
+									endContent={
+										<FaceIcon className='text-2xl text-default-400 pointer-events-none flex-shrink-0' />
+									}
+									label='Name'
+									placeholder='Enter your name'
+									variant='bordered'
+								/>
+								<Input
+									autoFocus
+									endContent={
+										<MailIcon className='text-2xl text-default-400 pointer-events-none flex-shrink-0' />
+									}
+									label='Email'
+									placeholder='Enter your email'
+									variant='bordered'
+								/>
+								<Input
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									endContent={
+										visible ? (
+											<VisibilityRoundedIcon
+												className='text-2xl text-default-400 pointer-events-none flex-shrink-0'
+												onClick={toggleVisibility}
+											/>
+										) : (
+											<VisibilityOffRoundedIcon
+												className='text-2xl text-default-400 pointer-events-none flex-shrink-0'
+												onClick={toggleVisibility}
+											/>
+										)
+									}
+									label='Password'
+									placeholder='Enter your password'
+									type={visible ? "text" : "password"}
+									variant='bordered'
+								/>
+								<div className='flex py-2 px-1 justify-between'>
+									<Checkbox
+										classNames={{
+											label: "text-small",
+										}}
+										onClick={toggleVisibility}
+									>
+										Show Password
+									</Checkbox>
+									<Link color='primary' href='#' size='sm'>
+										Forgot password?
+									</Link>
+								</div>
+								<Button color='primary' variant='shadow' onPress={onClose}>
+									Sign in
+								</Button>
+								<div className='flex py-2 px-1 justify-center gap-4'>
+									<h1> or join us with</h1>
+								</div>
+
+								<div className='flex py-2 px-1 justify-center gap-4'>
+									<GoogleIcon />
+									<GitHubIcon />
+								</div>
+								<div className='flex py-2 px-1 justify-center gap-4'>
+									<h1>Already have an account?</h1>
+									<Link href='/'>Sign in</Link>
+								</div>
+							</ModalBody>
+
+							<ModalFooter>
+								<Button color='danger' variant='flat' onPress={onClose}>
+									Close
+								</Button>
+							</ModalFooter>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
 		</NextUINavbar>
 	);
 };
