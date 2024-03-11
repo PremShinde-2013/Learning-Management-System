e795cf60303759fb77b1f63d22c351d90aa3f0c5
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-03-11 21:55:43 +0530

    started with course content and course ui

--- client/app/components/Admin/topbar/Course/CourseContent.tsx
+++ client/app/components/Admin/topbar/Course/CourseContent.tsx
@@ -1,5 +1,8 @@
 import React, { FC, useState } from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 
 type Props = {
 	activeStep: number;
@@ -16,11 +19,17 @@ const CourseContent: FC<Props> = ({
 	setActiveStep,
 	handleSubmit: handleCourseSubmit,
 }) => {
-	const [isCollapses, setIsCollapses] = useState(
+	const [isCollapsed, setIsCollapsed] = useState(
 		Array(courseContentData.length).fill(false)
 	);
 
 	const [activeSection, setActiveSection] = useState(1);
+
+	const handleCollapseToggle = (index: number) => {
+		const updatedCOllapsed = [...isCollapsed];
+		updatedCOllapsed[index] = !updatedCOllapsed[index];
+		setIsCollapsed(updatedCOllapsed);
+	};
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault();
@@ -34,7 +43,61 @@ const CourseContent: FC<Props> = ({
 						index === 0 ||
 						item.videoSection !== courseContentData[index - 1].videoSection;
 
-					return <></>;
+					return (
+						<>
+							<div
+								className={`w-full p-4 ${showSectionInput ? "mt-10" : "mb-0"}`}
+							>
+								<div className='flex w-full items-center justify-between my-0'>
+									{isCollapsed[index] ? (
+										<>
+											{item.title ? (
+												<p>
+													{index + 1}.{item.title}
+												</p>
+											) : (
+												<></>
+											)}
+										</>
+									) : (
+										<div></div>
+									)}
+
+									{/* arrow button  for collasped video content  */}
+									<div className='flex items-center'>
+										<DeleteRoundedIcon
+											className={`mr-2  ${
+												index > 0 ? "cursor-pointer" : "cursor-no-drop"
+											}`}
+											onClick={() => {
+												if (index > 0) {
+													const updatedData = [...courseContentData];
+													updatedData.splice(index, 1);
+													setCourseContentData(updatedData);
+												}
+											}}
+										/>
+										<ArrowDropDownRoundedIcon
+											fontSize='large'
+											style={{
+												transform: isCollapsed[index]
+													? "rotate(180deg)"
+													: "rotate(0deg)",
+											}}
+											onClick={() => handleCollapseToggle(index)}
+										/>
+									</div>
+								</div>
+								{!isCollapsed[index] && (
+									<>
+										<div className='my-3'>
+											<label>Video Title</label>
+										</div>
+									</>
+								)}
+							</div>
+						</>
+					);
 				})}
 			</form>
 		</div>
